bdc49a0b69074332c7d15acaab4c4e88a02de2a7
Julian Miguel Angle <dev.achmadjulian@gmail.com>
2025-04-17 07:36:30 +0700

    refactor: change coming-soon styles

--- src/routes/coming-soon/index.tsx
+++ src/routes/coming-soon/index.tsx
@@ -8,11 +8,11 @@ export default component$(() => {
 			<div class="h-screen w-screen px-4 sm:px-12 lg:px-16 flex items-center justify-center *:flex *:flex-col *:md:flex-row *:gap-y-6 *:lg:gap-x-12 *:items-start *:md:items-end relative">
 				<div class="absolute">
 					
-					<h1 class="text-[#67471D] heading">
+					<h1 class="font-playfair-display text-display-small sm:text-display-large font-medium text-primary-800">
 						Something <br />Brew-tiful <br />is Coming <br />Soon!
 					</h1>
 
-					<p class="text-[#3E3A37] subheading max-w-[600px]">
+					<p class="font-work-sans text-body-small sm:text-body-medium text-neutral-custom-700 max-w-[600px]">
 						Percikan aroma kopi spesialti segera hadir! Kami sedang melakukan final touch untuk memberikan pengalaman ngopi level tertinggi.
 					</p>
 
