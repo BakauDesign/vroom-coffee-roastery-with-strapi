9e52bd47ef0354d1ff80f025977d03b17150257c
Julian Miguel Angle <dev.achmadjulian@gmail.com>
2025-04-20 11:23:38 +0700

    fix: change singla value when menu item clicked

--- src/components/main/menu-item.tsx
+++ src/components/main/menu-item.tsx
@@ -49,7 +49,8 @@ export const MenuItem = component$<MenuItemProps>(({
                         ? "opacity-100 pointer-events-auto scale-100"
                         : "opacity-0 pointer-events-none scale-95"
                     }
-                `}>
+                `}
+                onClick$={() => isOpened.value = false}>
                     <Slot name='menu-item' />
                 </div>
             </div>
