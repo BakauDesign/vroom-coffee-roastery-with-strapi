3255e21873310b9c4c1bc47d4ea40afeec32d4e7
Julian Miguel Angle <dev.achmadjulian@gmail.com>
2025-04-20 09:14:23 +0700

    fix: add full width

--- src/components/main/separator.tsx
+++ src/components/main/separator.tsx
@@ -6,7 +6,7 @@ import Separator1000 from "~/assets/Separator 1000.png";
 export const Separator = component$(() => {
     return (
         <>
-            <div class="flex overflow-hidden *:w-full *:h-[3px]">
+            <div class="w-full flex overflow-hidden *:w-full *:h-[3px]">
                 <img src={Separator400} alt="Separator 400" class="sm:hidden" />
                 <img src={Separator1000} alt="Separator 1000" class="hidden sm:block" />
             </div>
