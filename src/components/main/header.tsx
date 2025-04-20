7b52a63eb5ddf0858f516ff289fd94f4a3d9cb13
Julian Miguel Angle <dev.achmadjulian@gmail.com>
2025-04-20 09:16:31 +0700

    fix: add z-index 20

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -11,7 +11,7 @@ export const Header = component$(() => {
     const isOpen = useSignal(false);
 
     return (
-        <header class="w-full h-[100px] py-4 px-4 sm:px-6 lg:px-[60px] flex gap-x-12 items-center bg-primary-base border-[3px] border-solid border-neutral-custom-200 fixed">
+        <header class="w-full h-[100px] py-4 px-4 sm:px-6 lg:px-[60px] flex gap-x-12 items-center bg-primary-base border-[3px] border-solid border-neutral-custom-200 fixed z-20">
             <img alt="Logo" class="h-[60px] min-w-[120px] sm:w-[150px] lg:w-[200px] flex items-center justify-center bg-primary-50 rounded-[4px]" />
 
             <section class="w-full flex items-center justify-end xl:justify-center *:gap-x-6 *:py-2 *:px-3 *:bg-primary-50 *:rounded-full">
