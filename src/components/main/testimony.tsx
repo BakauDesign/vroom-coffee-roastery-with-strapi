c1fe1cadba7bca2d44bc04152526a7468bf8cbf0
Julian Miguel Angle <dev.achmadjulian@gmail.com>
2025-05-23 13:30:24 +0700

    fix: add key and height, width attribute

--- src/components/main/testimony.tsx
+++ src/components/main/testimony.tsx
@@ -29,7 +29,7 @@ export const Testimony = component$<{ data: TestimonyProps }>(({
         >
             <section class="flex flex-col gap-y-4">
                 <figure class="flex flex-col gap-y-8">
-                    <img src={avatar} alt={`${name} avatar`} class="h-[100px] w-[100px] object-cover rounded-[4px] bg-primary-100" />
+                    <img src={avatar} alt={`${name} avatar`} height={100} width={100} class="h-[100px] w-[100px] object-cover rounded-[4px] bg-primary-100" />
 
                     <figcaption class="font-lora font-semibold text-h2-small sm:text-h2-medium">
                         {name},&nbsp;{location}
@@ -49,7 +49,7 @@ export const Testimony = component$<{ data: TestimonyProps }>(({
                 <ul class="flex flex-col gap-y-3 *:w-fit *:py-1.5 *:px-4 *:bg-primary-50 *:rounded-[4px] *:text-primary-600">
                     {purchasedProducts.map((product) => {
                         return (
-                            <li id={`${product.id}`}>
+                            <li key={product.id} id={`${product.id}`}>
                                 {product.name}
                             </li>
                         );
