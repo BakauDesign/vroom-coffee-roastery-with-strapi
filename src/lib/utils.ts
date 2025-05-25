f521599c884655d1b3d82c5db61261a1a2226d47
Julian Miguel Angle <dev.achmadjulian@gmail.com>
2025-05-25 18:34:44 +0700

    feat: add format currency utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function formatRupiah(number: number) {
+    return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    }).format(number);
+}
+
+export function rupiahToDecimal(rupiah: string): number {
+    const numeric = rupiah
+        .replace(/[^0-9,-]+/g, '')
+        .replace(',', '.');
+
+    return parseFloat(numeric);
+}
