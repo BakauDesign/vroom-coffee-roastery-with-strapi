45617d555d9ce795db6cf886c41d1d4987f9dc90
BakauDesign <hi.bakaudesign@gmail.com>
2025-06-14 20:44:12 +0700

    feat: add filter option for roasted beans

---
+++ src/lib/filter-option.ts
@@ -0,0 +1,12 @@
+export const roastedFilterOption = {
+    brewingMethod: [
+        { id: 1, label: "Pour Over" },
+        { id: 2, label: "French Press" },
+        { id: 3, label: "Aeropress" },
+        { id: 4, label: "Espresso" },
+        { id: 5, label: "Moka Pot" },
+        { id: 6, label: "Cold Brew" },
+        { id: 7, label: "Siphon" },
+        { id: 8, label: "Semua Metode" }
+    ]
+};
