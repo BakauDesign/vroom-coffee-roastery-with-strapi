df9c2fdc4f028bcceb5aceef9f014cb3a539b74c
BakauDesign <hi.bakaudesign@gmail.com>
2025-06-14 14:12:23 +0700

    fix: remove /edge endpoint

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,9 @@
-// import { PrismaClient as PrismaClient } from "@prisma/client";
-// import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge";
-// import { PrismaD1 } from "@prisma/adapter-d1";
 import { isDev } from "@builder.io/qwik";
 
 export async function getDB(env: { DB: D1Database }) {
   if (!isDev && env.DB) {
     const { PrismaD1 } = await import('@prisma/adapter-d1');
-    const { PrismaClient } = await import('@prisma/client/edge');
+    const { PrismaClient } = await import('@prisma/client');
     const adapter = new PrismaD1(env.DB);
     return new PrismaClient({ adapter });
   } else {
