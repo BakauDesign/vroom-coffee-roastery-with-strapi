5a20f6438810259fc19cf7314ef74758b1b5153b
BakauDesign <hi.bakaudesign@gmail.com>
2025-06-18 03:14:32 +0700

    feat: add user schema

---
+++ src/schema/user.ts
@@ -0,0 +1,34 @@
+import * as v from 'valibot';
+
+export const UserSchema = v.object({
+    name: v.pipe(
+        v.string(),
+        v.nonEmpty('Mohon masukan nama kamu')
+    ),
+    username: v.pipe(
+        v.string(),
+        v.nonEmpty('Mohon masukan username kamu')
+    ),
+    avatar: v.any(),
+    role: v.pipe(
+        v.string(),
+        v.nonEmpty('Mohon pilih role kamu')
+    ),
+    password: v.pipe(
+        v.string(),
+        v.nonEmpty('Mohon masukan password kamu'),
+        v.minLength(8, 'Panjang password harus 8 karakter atau lebih'),
+    ),
+});
+
+export const UserAvatarSchema = v.pipe(
+    v.nullable(
+        v.pipe(
+            v.file(),
+            v.mimeType(['image/jpeg', 'image/jpg', 'image/png', 'image/avif'], 'Mohon pilih gambar dengan format JPEG atau PNG.'),
+            v.maxSize(1024 * 1024 * 10, 'Mohon pilih gambar berukuran kurang dari 10 MB.')
+        )
+    )
+);
+
+export type UserForm = v.InferInput<typeof UserSchema>;
