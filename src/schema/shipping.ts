ca44140dd1f583da913bb661ef56a60164696d03
BakauDesign <hi.bakaudesign@gmail.com>
2025-06-20 22:39:46 +0700

    feat: add shipping validation schema

---
+++ src/schema/shipping.ts
@@ -0,0 +1,32 @@
+import * as v from 'valibot';
+
+export const ShippingSchema = v.object({
+    id: v.nullable(
+        v.pipe(
+            v.number()
+        )
+    ),
+    name: v.pipe(
+        v.string(),
+        v.nonEmpty('Mohon masukan nama pengiriman')
+    ),
+    logo: v.any(),
+    cost: v.pipe(
+        v.number('Mohon masukan biaya pengiriman')
+    ),
+    status: v.pipe(
+        v.boolean('Mohon pilih status pengiriman')
+    )
+});
+
+export const ShippingLogoSchema = v.pipe(
+    v.nullable(
+        v.pipe(
+            v.file(),
+            v.mimeType(['image/jpeg', 'image/jpg', 'image/png', 'image/avif'], 'Mohon pilih gambar dengan format JPEG atau PNG.'),
+            v.maxSize(1024 * 1024 * 5, 'Mohon pilih gambar berukuran kurang dari 5 MB.')
+        )
+    )
+);
+
+export type ShippingForm = v.InferInput<typeof ShippingSchema>;
