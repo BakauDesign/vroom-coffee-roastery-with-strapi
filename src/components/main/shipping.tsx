7f4f65f723baefeb34f9f820046d38ea3ebe5294
BakauDesign <hi.bakaudesign@gmail.com>
2025-07-01 08:01:03 +0700

    feat: add cursor pointer class utilities

--- src/components/main/shipping.tsx
+++ src/components/main/shipping.tsx
@@ -30,8 +30,8 @@ Shipping.Root = component$((props) => {
         <figure 
             {...props}
             class={`
-                py-4 px-5 min-w-[180px] bg-primary-base rounded-[12px] flex gap-x-6 items-center
-                border-2 ${props.selected ? "border-primary-200" : "border-neutral-100"}
+                py-4 px-5 min-w-[180px] bg-primary-base rounded-[12px] flex gap-x-6 items-center cursor-pointer
+                border-2 ${props.selected ? "border-primary-200" : "border-neutral-200"}
             `}>
             <Slot />
         </figure>
@@ -63,7 +63,7 @@ Shipping.Name = component$(() => {
 Shipping.Cost = component$(() => {
     return (
         <h2 class="text-primary-700 text-label-small sm:text-label-medium font-medium font-work-sans">
-            Rp. <Slot />
+            <Slot />
         </h2>
     );
 });
