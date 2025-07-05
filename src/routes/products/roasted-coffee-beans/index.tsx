9e99476f070964fad39b63cbc5a2c7cbca6f0687
BakauDesign <hi.bakaudesign@gmail.com>
2025-07-05 09:52:09 +0700

    feat: release roasted-coffee-beans main page

--- src/routes/products/roasted-coffee-beans/index.tsx
+++ src/routes/products/roasted-coffee-beans/index.tsx
@@ -1,5 +1,8 @@
-import { component$, isDev } from "@builder.io/qwik";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import {
+    component$,
+    // isDev
+} from "@builder.io/qwik";
+// import type { RequestHandler } from "@builder.io/qwik-city";
 
 // import { Button } from "~/components/main/button";
 import { Gradient } from "~/components/main/gradient";
@@ -8,17 +11,30 @@ import { Separator } from "~/components/main/separator";
 // import Keberlanjutan from "~/assets/Icons/Keberlanjutan.png";
 // import Transparansi from "~/assets/Icons/Transparansi.png";
 // import Inovasi from "~/assets/Icons/Inovasi.png";
-import { Input } from "~/components/main/input";
-import { TextField } from "~/components/main/text-field";
-import { Shipping } from "~/components/main/shipping";
 
-export const onGet: RequestHandler = async ({ redirect }) => {
-	if (!isDev) {
-		throw redirect(302, "/coming-soon");		
-	}
-};
+import HeroImage_1 from "~/assets/main/products/roasted-coffee-beans/Hero image 1.avif";
+import HeroImage_2 from "~/assets/main/products/roasted-coffee-beans/Hero image 2.avif";
+import HeroImage_3 from "~/assets/main/products/roasted-coffee-beans/Hero image 3.avif";
+
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { getProducts } from "~/server/services/products";
+import { Product } from "~/components/main/product";
+
+// export const onGet: RequestHandler = async ({ redirect }) => {
+// 	if (!isDev) {
+// 		throw redirect(302, "/coming-soon");		
+// 	}
+// };
+
+export const useProducts = routeLoader$(
+    async ( event ) => {
+        return await getProducts({ event });
+    }
+)
 
 export default component$(() => {
+    const { value: products } = useProducts();
+
     return (
         <>
             <figure class="hero-section ">
@@ -34,11 +50,11 @@ export default component$(() => {
                     </article>
                 </figcaption>
 
-                <section class="hero-image grid-cols-2 lg:grid-cols-4 max-h-[500px]">
+                <section class="hero-image grid-cols-2 lg:grid-cols-4 *:aspect-square">
                     <div class="hidden lg:block" />
-                    <img src="https://i.pinimg.com/736x/a1/cd/44/a1cd44f6617beebb9794877ef59082a1.jpg" alt="Hero image 1" height={500} width={500} />
-                    <img src="https://i.pinimg.com/736x/a1/cd/44/a1cd44f6617beebb9794877ef59082a1.jpg" alt="Hero image 2" height={500} width={500} />
-                    <img src="https://i.pinimg.com/736x/a1/cd/44/a1cd44f6617beebb9794877ef59082a1.jpg" alt="Hero image 3" height={500} width={500} />
+                    <img src={HeroImage_1} alt="Hero image 1" height={500} width={500} />
+                    <img src={HeroImage_2} alt="Hero image 2" height={500} width={500} />
+                    <img src={HeroImage_3} alt="Hero image 3" height={500} width={500} />
                 </section>
             </figure>
 
@@ -49,62 +65,24 @@ export default component$(() => {
                 <Gradient position="bottom" />
 
                 <section class="general-section gap-y-[60px] items-center">
-                    <Input.Root>
-                        <Input.Header>
-                            <Input.Label>Nama Lengkap</Input.Label>
-                            <Input.Option>(Wajib)</Input.Option>
-                        </Input.Header>
-                    
-                        <Input.FieldText
-                            placeholder="cth: Jokowi Nipunegoro"
-                            onChange$={(event$) => console.info(event$.target)}
-                        />
-
-                        <Input.Message>
-                            Failed
-                        </Input.Message>
-                    </Input.Root>
-
-                    <Input.Root>
-                        <Input.Header>
-                            <Input.Label>Nama Lengkap</Input.Label>
-                            <Input.Option>(Wajib)</Input.Option>
-                        </Input.Header>
-                    
-                        <Input.FieldPassword
-                            placeholder="cth: Jokowi Nipunegoro"
-                            onChange$={(event$) => console.info(event$.target)}
-                        />
-
-                        <Input.Message>
-                            Failed
-                        </Input.Message>
-                    </Input.Root>
-
-                    <TextField.Root>
-                        <TextField.Header>
-                            <TextField.Label>Nama Lengkap</TextField.Label>
-                            <TextField.Option>(Wajib)</TextField.Option>
-                        </TextField.Header>
-                    
-                        <TextField.Field
-                            placeholder="cth: Jokowi Nipunegoro"
-                            onChange$={(event$) => console.info(event$.target)}
-                        />
-
-                        <TextField.Message>
-                            Failed
-                        </TextField.Message>
-                    </TextField.Root>
-
-                    <Shipping.Root onClick$={() => alert("JNE")}>
-                        <Shipping.Logo src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/New_Logo_JNE.png/1200px-New_Logo_JNE.png" />
-
-                        <Shipping.Detail>
-                            <Shipping.Name>JNE</Shipping.Name>
-                            <Shipping.Cost>13000</Shipping.Cost>
-                        </Shipping.Detail>
-                    </Shipping.Root>
+                    <section class="grid gap-9 overflow-scroll grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+                        {products.data.map((product) => {
+                            return (
+                                <Product
+                                    key={product.id}
+                                    id={product.id}
+                                    name={product.name}
+                                    description={product.description}
+                                    type={product.type}
+                                    price={product.price}
+                                    discount={product.discount}
+                                    discountPrice={product.discount_price}
+                                    photo={product.photo}
+                                    weight={product.weight}
+                                />
+                            )
+                        })}
+                    </section>
                 </section>
             </div>
 
